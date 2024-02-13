986bbf22bd267b5979621833f8eb91120e56e895
Rajinder Singh <rajinder.think360@gmail.com>
2024-02-13 11:23:13 +0530

    Fixed form implementation

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,17 @@
 from flask import Flask
 
 app = Flask(__name__,template_folder='templates')
+#print(app.config)
+
+# if app.config["ENV"] == "production":
+#     app.config.from_object("config.Production")
+# elif app.config["ENV"] == "testing":
+#     app.config.from_object("config.Testing")
+# else:
+    #app.config.from_object("config.Develoment")
+
+app.config.from_object("config.Production")
+#app.config.from_object("config.Develoment")
 
 from app import frontViews
-
 from app import adminViews
--- app/frontViews.py
+++ app/frontViews.py
@@ -1,8 +1,23 @@
 from app import app
 
-from flask import render_template
+from flask import render_template, request, redirect
 
 @app.route("/")
 def index():
     return render_template("front/index.html")
     #return "<p>Hello, Front View!</p>"
+
+@app.route("/getInTuch",  methods=["GET", "POST"])
+def getInTuch():
+    if request.method == "POST":
+        yourName = request.form.get("yourName")
+        email = request.form.get("email")
+        phoneNumber = request.form.get("phoneNumber")
+        message = request.form.get("message")
+        print(yourName)
+    return render_template("front/index.html")
+
+
+@app.route("/form",  methods=["GET", "POST"])
+def form():
+    return render_template("front/form.html")
